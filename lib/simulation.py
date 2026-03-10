313535fbc71fc038da5f7dd6db8b8639a2a9112a
Arnab Majumdar <arnab.majumdar@hereon.de>
2026-03-10 12:41:42 +0100

    added all chem evo models

--- lib/simulation.py
+++ lib/simulation.py
@@ -50,6 +50,8 @@ def model_run(sim_model,nodes, midpoint, t, t_end):
         sim_sld, sld_max, sld_min = model_sld_grow(nodes, midpoint, t, t_end)
     if sim_model=='phase_field':
         sim_sld, sld_max, sld_min = model_phase_field()
+    if sim_model=='chemevo':
+        sim_sld, sld_max, sld_min = model_chemevo(nodes, midpoint, t, t_end)
     return sim_sld, sld_max, sld_min
 
 def model_phase_field():
@@ -245,4 +247,81 @@ def model_sld_grow(nodes, midpoint, t, t_end):
     sim_sld [cord_ed <= rad**2] = sld_grain
     sld_max=max(sld_start, sld_end, sld_env)
     sld_min=min(sld_start, sld_end, sld_env)
+    return sim_sld, sld_max, sld_min
+
+def model_chemevo(nodes, midpoint, t, t_end):
+    """
+    Function desc:
+    define sld, man and min for chem_evo model 
+    """
+    # read model_run_param from xml
+    xml_folder='./xml/'
+    struct_xml=os.path.join(xml_folder, 'model_chemevo.xml')
+    tree=ET.parse(struct_xml)
+    root = tree.getroot()
+    # read params
+    mode=root.find('mode').text
+    rad=float(root.find('rad').text)
+    sld_start=float(root.find('sld_start').text)
+    sld_end=float(root.find('sld_end').text)
+    sld_env=float(root.find('sld_env').text)
+    # run simulation
+    nodes = np.array(nodes)
+    cord_ed = np.sum((nodes-midpoint)**2,axis=1)
+    sim_sld = sld_env*np.ones(len(nodes))
+    if mode=='homo':
+        sld_grain=sld_start+t*(sld_end-sld_start)/t_end
+        sim_sld [cord_ed <= rad**2] = sld_grain
+        sld_max=max(sld_start, sld_end, sld_env)
+        sld_min=min(sld_start, sld_end, sld_env)
+    elif mode=='step':
+        rad_out=rad
+        rad_in=rad-t*(rad/t_end)
+        rad_in=max(rad_in,0)
+        sim_sld [cord_ed <= rad_out**2] = sld_end
+        sim_sld [cord_ed <= rad_in**2] = sld_start
+        sld_max=max(sld_start, sld_end, sld_env)
+        sld_min=min(sld_start, sld_end, sld_env)
+    elif mode=='diffuse':
+        n=50 # num_term in series
+        D_coeff=float(root.find('d_coeff').text)
+        r=np.sqrt(cord_ed)
+        a=rad
+        c1= sld_start
+        c0= sld_end
+        if t==0:
+            sim_sld [r**2 <= a**2] = c1
+        else:
+            if D_coeff*t<=0.1:
+                for i in enumerate(r):
+                    series=0
+                    if r[i] <=rad:
+                        if r[i]==0:
+                            for j in range(1,n):
+                                series+=((-1)**j) * np.exp(-((D_coeff*(j**2)*(np.pi**2)*t)/(a**2)))
+                        
+                            sim_sld[i]=c1+(c0-c1)*(1+2*series)
+                        else:
+                            for j in range(n):
+                                #series+=((-1)**j/j) * np.sin(j*np.pi*r[i]/a) * np.exp(-((D_coeff*(j**2)*(np.pi**2)*t)/(a**2)))
+                                term_1=(2*j+1)*a
+                                term_2=2* np.sqrt(D_coeff*t)
+                                series+=erf((term_1-r[i])/term_2)-erf((term_1+r[i])/term_2)
+                            sim_sld[i]=c1+(c0-c1)*(a/r[i]*series)
+            else:
+                for i in enumerate(r):
+                    series=0
+                    if r[i] <=rad:
+                        if r[i]==0:
+                            for j in range(1,n):
+                                series+=((-1)**j) * np.exp(-((D_coeff*(j**2)*(np.pi**2)*t)/(a**2)))                       
+                            sim_sld[i]=c1+(c0-c1)*(1+2*series)
+                        else:
+                            for j in range(1,n):
+                                series+=((-1)**j/j) * np.sin(j*np.pi*r[i]/a) * np.exp(-((D_coeff*(j**2)*(np.pi**2)*t)/(a**2)))                      
+                            sim_sld[i]=c1+(c0-c1)*(1+((2*a)/(np.pi*r[i]))*series)
+        sld_max=max(sld_start, sld_end, sld_env)
+        sld_min=min(sld_start, sld_end, sld_env)
+    else:
+        print('problem with mode in chem evo xml')
     return sim_sld, sld_max, sld_min
